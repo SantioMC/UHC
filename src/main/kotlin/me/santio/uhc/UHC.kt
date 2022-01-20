eb2249413b7dcc4fc56e1e8ae765adce4ca8c98d
Santio <santiothebest123@gmail.com>
2022-01-20 18:32:45 -0500

    Fix chat not being formatted correctly on vanilla server

--- src/main/kotlin/me/santio/uhc/UHC.kt
+++ src/main/kotlin/me/santio/uhc/UHC.kt
@@ -3,12 +3,10 @@ package me.santio.uhc
 import me.santio.uhc.exceptions.DuplicateScenarioException
 import me.santio.uhc.listeners.ChatListener
 import me.santio.uhc.listeners.JoinListener
-import me.santio.uhc.scenarios.DoubleDropsScenario
 import me.santio.uhc.utils.FreezeUtils
 import me.santio.uhc.utils.FreezeUtils.Companion.reset
 import net.md_5.bungee.api.ChatColor
 import org.bukkit.Bukkit
-import org.bukkit.Location
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -42,8 +40,8 @@ class UHC : JavaPlugin() {
     }
 
     companion object {
-        val mainColor = ChatColor.of("#6666f9")
-        var lobby = Location(Bukkit.getWorlds()[0], 0.0, 1.0, 0.0)
+        val mainColor = ChatColor.of("#66b4f9")
+        var lobby = Bukkit.getWorlds()[0].spawnLocation.add(0.5, 0.0, 0.5)
 
         fun get(): UHC {
             return getPlugin(UHC::class.java)
--- src/main/kotlin/me/santio/uhc/listeners/ChatListener.kt
+++ src/main/kotlin/me/santio/uhc/listeners/ChatListener.kt
@@ -1,8 +1,10 @@
 package me.santio.uhc.listeners
 
 import me.santio.uhc.Game
+import me.santio.uhc.UHC
 import net.md_5.bungee.api.chat.HoverEvent
 import net.md_5.bungee.api.chat.TextComponent
+import net.md_5.bungee.api.chat.hover.content.Text
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
@@ -16,15 +18,15 @@ object ChatListener : Listener {
     fun onChat(event: AsyncPlayerChatEvent) {
         if (!event.isCancelled) {
             val gamePlayer = Game.players[event.player.uniqueId] ?: return
-            val hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, gamePlayer.chatHover)
 
             val original: String = try { String.format(event.format, event.player.name, event.message) }
             catch(e: MissingFormatArgumentException) { event.format }
-            val component = TextComponent.fromLegacyText(event.format)
-            component[0].hoverEvent = hoverEvent
+
+            val component = TextComponent.fromLegacyText(original)
+            component[0].hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, Text(gamePlayer.chatHover))
 
             event.isCancelled = true
-            Bukkit.getOnlinePlayers().forEach { player: Player? -> player!!.spigot().sendMessage(*component) }
+            Bukkit.getOnlinePlayers().forEach { player: Player -> player.spigot().sendMessage(*component) }
         }
     }
 }
